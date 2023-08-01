754cdf539ee45cd8230d01b93dfc14db11000fa6
InnaMcGrane <innamcgrane@gmail.com>
2023-08-01 16:58:14 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App(){
+  const[books,setBooks] = useState(data);
+   const [showText, setShowText] = useState(false);
+
+  const removeBook = (id) => {
+    let newBooks = books.filter((book)=> book.id !== id);
+    setBooks(newBooks)
+
+  }
+
+  const showTextClick = (item) => {
+    item.showMore =!item.showMore
+    setShowText(!showText)
+  }
+
+  return(<div>
+    <div className="container">
+      <h1> Top {books.length} Books I recommend reading</h1>
+    </div>
+
+    {books.map((item => {
+      const {id, bookName, description, image, author, showMore} = item;
+    
+
+      return (
+        <div key={id}>
+          <div className="container">
+            <h2>
+              {id} - {bookName}
+            </h2>
+          </div>
+
+          <div className="container">
+            <p>
+              {showMore ? description : description.substring(0, 200) + "...."}
+              <button onClick={() => showTextClick(item)}>{showMore ? "show less" : "show more"}</button>
+            </p>
+          </div>
+
+          <div className="container">
+            <img src={image} width="350px" alt="book" />
+          </div>
+
+          <div className="container">
+            <p> {author} </p>
+          </div>
+
+          <div className="container">
+            <button className="btn" onClick={() => removeBook(id)}>
+              remove
+            </button>
+          </div>
+        </div>
+      );
+
+    } ))}
+  </div>)
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    id: 1,
+
+    bookName: "Promises At Dawn",
+
+    description:
+      "Romain Gary tells the story of his childhood and youth with his mother, a Russian former actress carried by a love and unconditional faith in her son. The story, full of humor and tenderness, tells the story of her tireless battle against adversity, the extravagant energy she deploys so that he attain to a great destiny, and the efforts of Romain, who is willing to do all to make his life coincide with the naive dream of the one he loves",
+
+    image:
+      "https://images.unsplash.com/photo-1599276188787-63e64b366e9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzR8fGJvb2tzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+
+    author: "Romain Gary",
+
+    showMore: false
+  },
+
+  {
+    id: 2,
+
+    bookName: "We The living",
+
+    description:
+      "We the Living is the debut novel of the Russian American novelist Ayn Rand. It is a story of life in post-revolutionary Russia and was Rand's first statement against communism. Rand observes in the foreword that We the Living was the closest she would ever come to writing an autobiography. Rand finished writing the novel in 1934, but it was rejected by several publishers before being released by Macmillan Publishing in 1936.[1] It has since sold more than three million copies.",
+
+    image:
+      "https://images.unsplash.com/photo-1512903491135-76ec8a4510b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA0fHx3ZSUyMHRoZSUyMGxpdmluZyUyMGJvb2t8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+
+    author: "Ayn Rand",
+
+    showMore: false
+  },
+
+  {
+    id: 3,
+
+    bookName: "Gone With The Wind",
+
+    description:
+      "Gone with the Wind is a novel by American writer Margaret Mitchell, first published in 1936. The story is set in Clayton County and Atlanta, both in Georgia, during the American Civil War and Reconstruction Era. It depicts the struggles of young Scarlett O'Hara, the spoiled daughter of a well-to-do plantation owner, who must use every means at her disposal to claw her way out of poverty following Sherman's destructive March to the Sea. This historical novel features a coming-of-age story, with the title taken from the poem Non Sum Qualis eram Bonae Sub Regno Cynarae, written by Ernest Dowson.",
+
+    image:
+      "https://images.unsplash.com/photo-1562232573-0305012a8818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAxfHx3ZSUyMHRoZSUyMGxpdmluZyUyMGJvb2t8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+
+    author: "Margaret Mitchel",
+
+    showMore: false
+  },
+];
